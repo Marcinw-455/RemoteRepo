2f22f742fcbc1d82def9fcb04b8c957a3cad9947
Mateusz Kuriata <mateusz.kuriata@eclipsegroup.co.uk>
2021-03-03 16:07:53 +0100

    test issue

--- app-server-express/apka.js
+++ app-server-express/apka.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = new express();
+const port = 3400;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-    res.sendFile('/home/marcin/Desktop/Repos/RemoteRepo/app-server-express/index.html');
+    res.sendFile(path.join(__dirname,'index.html'));
 }).listen(11000);
+app.listen(3000, function() { console.log('listening'); });
